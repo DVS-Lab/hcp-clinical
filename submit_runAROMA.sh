af198189030d4f1b7d77be46890db707baecb06b
DVSneuro <david.v.smith@temple.edu>
2018-08-21 17:37:51 -0400

    initial commit

---
+++ submit_runAROMA.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+for task in WM SOCIAL EMOTION GAMBLING; do
+  for subj in `cat newsubs`; do
+  	for RUN in LR RL; do
+
+  		#Manages the number of jobs and cores
+  		SCRIPTNAME=runAROMA.sh
+  		NCORES=24
+  		while [ $(ps -ef | grep -v grep | grep $SCRIPTNAME | wc -l) -ge $NCORES ]; do
+  	  		sleep 1m
+  		done
+  		bash runAROMA.sh $task $RUN $subj &
+  		sleep 5s
+
+  	done
+  done
+done
