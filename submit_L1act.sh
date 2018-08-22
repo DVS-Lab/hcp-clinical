473950d6e0129d2088a5194391f216db4ebc60c6
DVSneuro <david.v.smith@temple.edu>
2018-08-22 18:23:40 -0400

    initial commit

---
+++ submit_L1act.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+for task in Emo Soc; do
+  for subj in `cat newsubs`; do
+  	for RUN in LR RL; do
+
+  		#Manages the number of jobs and cores
+  		SCRIPTNAME=L1_${task}_Act.sh
+  		NCORES=20
+  		while [ $(ps -ef | grep -v grep | grep $SCRIPTNAME | wc -l) -ge $NCORES ]; do
+  	  		sleep 1m
+  		done
+  		bash $SCRIPTNAME $RUN $subj &
+  		sleep 5s
+
+  	done
+  done
+done
