20d7ba784fa2ea364d235fdf54b8a34d85b8200c
DVSneuro <david.v.smith@temple.edu>
2018-09-03 16:13:35 -0400

    update L2 files

---
+++ submit_L2act.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+for task in WM Gam; do
+  for subj in `cat Michelle_AllSubs_n146.txt`; do
+
+    #Manages the number of jobs and cores
+    SCRIPTNAME=L2_${task}_Act.sh
+    NCORES=12
+    while [ $(ps -ef | grep -v grep | grep $SCRIPTNAME | wc -l) -ge $NCORES ]; do
+      sleep 1m
+    done
+    bash $SCRIPTNAME $subj &
+    sleep 5s
+
+  done
+done
+
+
