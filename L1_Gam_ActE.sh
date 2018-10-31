5cc253e904612c05d7595d33fe1e999d48f65f6c
mchiu91 <tuh16547@temple.edu>
2018-10-31 15:37:14 -0400

    template and level 1 and 2 code modified for event design of gambling

---
+++ L1_Gam_ActE.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+basedir=`pwd`
+MAINDATADIR=/data/projects/ppi-effect-sizes/data
+MAINOUTPUTDIR=/data/projects/ppi-effect-sizes/fsl
+
+task=GAMBLING
+run=$1
+subj=$2
+
+OUTPUT=${MAINOUTPUTDIR}/${subj}/MNINonLinear/Results/tfMRI_${task}_${run}/L1_Gam_ActE
+DATA=${MAINOUTPUTDIR}/${subj}/MNINonLinear/Results/tfMRI_${task}_${run}/smoothing.feat/ICA_AROMA/denoised_func_data_nonaggr.nii.gz
+NVOLUMES=`fslnvols ${DATA}`
+
+# checking L1 output
+if [ -e ${OUTPUT}.feat/cluster_mask_zstat1.nii.gz ]; then
+  #echo "output exists, skipping...."
+  exit
+else
+  rm -rf ${OUTPUT}.feat
+fi
+
+#EV files
+EVWIN=${MAINDATADIR}/${subj}/MNINonLinear/Results/tfMRI_${task}_${run}/EVs/win_event.txt
+EVLOSS=${MAINDATADIR}/${subj}/MNINonLinear/Results/tfMRI_${task}_${run}/EVs/loss_event.txt
+EVNEUT=${MAINDATADIR}/${subj}/MNINonLinear/Results/tfMRI_${task}_${run}/EVs/neut_event.txt
+
+#find and replace: run feat for smoothing
+ITEMPLATE=${basedir}/templates/L1_Gam_ActE.fsf
+OTEMPLATE=${MAINOUTPUTDIR}/${subj}/MNINonLinear/Results/tfMRI_${task}_${run}/L1_Gam_ActE.fsf
+sed -e 's@OUTPUT@'$OUTPUT'@g' \
+-e 's@DATA@'$DATA'@g' \
+-e 's@NVOLUMES@'$NVOLUMES'@g' \
+-e 's@EVWIN@'$EVWIN'@g' \
+-e 's@EVLOSS@'$EVLOSS'@g' \
+-e 's@EVNEUT@'$EVNEUT'@g' \
+<$ITEMPLATE> $OTEMPLATE
+feat $OTEMPLATE
+
+# fix registration
+rm -rf ${OUTPUT}.feat/reg
+mkdir -p ${OUTPUT}.feat/reg
+ln -s $FSLDIR/etc/flirtsch/ident.mat ${OUTPUT}.feat/reg/example_func2standard.mat
+ln -s $FSLDIR/etc/flirtsch/ident.mat ${OUTPUT}.feat/reg/standard2example_func.mat
+ln -s $FSLDIR/data/standard/MNI152_T1_2mm.nii.gz ${OUTPUT}.feat/reg/standard.nii.gz
+
+#delete unused files
+#rm -rf ${OUTPUT}.feat/filtered_func_data.nii.gz
+rm -rf ${OUTPUT}.feat/stats/res4d.nii.gz
+rm -rf ${OUTPUT}.feat/stats/corrections.nii.gz
+rm -rf ${OUTPUT}.feat/stats/threshac1.nii.gz
